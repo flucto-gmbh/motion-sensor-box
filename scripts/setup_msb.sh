df845271f918f2fd0bf76cdc3a94222af6b0eb2a
Aljoscha Sander <aljoscha.sander@mailbox.org>
2022-06-13 14:47:40 +0200

    updated setupt_msb.sh

--- scripts/setup_msb.sh
+++ scripts/setup_msb.sh
@@ -8,6 +8,13 @@ SCRIPT_DIR=$(dirname $0)
 MSB_BASE_DIR="${PWD}/${SCRIPT_DIR}/.."
 MSB_CONFIG_DIR="${PWD}/${SCRIPT_DIR}/../config"
 MSB_SRC_DIR="${PWD}/${SCRIPT_DIR}/../src"
+MSB_SSH_KEY="${HOME}/.ssh/msb_key"
+
+if ! [[ -f "${MSB_SSH_KEY}" ]]
+then
+  echo "could not find ssh key at ${MSB_SSH_KEY}"
+  exit
+fi
 
 function update_software() {
   sudo apt update && sudo apt upgrade -y
@@ -31,18 +38,36 @@ function copy_raspiconfig () {
   sudo cp "${MSB_CONFIG_DIR}/cmdline.txt" /boot/
 }
 
-function insert_port () {
+function get_port_from_hostname () {
   serial=$(hostname | cut -d "-" -f2 | tr "0" " ") 
   port=$(python -c "port=65000+${serial}; print(f'{port}')")
+  echo "${port}"
+}
+
+function insert_port () {
+  port=$(get_port_from_hostname)
   sudo sed -i "s/\[REMOTE_PORT\]/${port}/" /etc/systemd/system/rtunnel.service 
+}
+
+function check_ssh () {
+  server_response=$(ssh \
+    -i "${MSB_SSH_KEY}"\
+    -o "StrictHostKeyChecking no"\
+    -p $(get_port_from_hostname)\
+    -l msb\
+    flucto.tech \
+    "hostname")
 }
 
 function setup_rtunnel () {
   sudo cp "${MSB_CONFIG_DIR}"/services/rtunnel.service /etc/systemd/system/
   insert_port
+  check_ssh
+  sudo systemctl enable rtunnel.service
+  sudo systemctl start rtunnel.service
 }
 
-function main () {
+function setup_msb () {
   #update_software
   #install_dependencies
   #install_python_requirements
@@ -50,4 +75,4 @@ function main () {
   #copy_raspiconfig
 }
 
-main
+setup_msb
