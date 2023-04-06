8717ed96238f0e1dc23f2b547fae9b05dbaa782c
Aljoscha Sander <aljoscha.sander@mailbox.org>
2023-04-06 16:10:58 +0800

    modified Publisher and Subscriber class to handle configuration automatically

--- msb/zmq_base/Config.py
+++ msb/zmq_base/Config.py
@@ -22,3 +22,18 @@ class SubscriberConfig:
     def connect_to(self): 
         return f"{self.protocol}://{self.address}:{self.port}"
 
+@dataclass
+class ZMQConf:
+    protocol : str = "tcp"
+    address : str = "127.0.0.1"
+    publisher_port : int = 5555
+    subscriber_port : int = 5556
+    packer : str = "json"
+
+    @property
+    def connect_to_publisher(self):
+        return f"{self.protocol}://{self.address}:{self.publisher_port}"
+
+    @property
+    def connect_to_subscriber(self):
+        return f"{self.protocol}://{self.address}:{self.subscriber_port}"
--- msb/zmq_base/Subscriber.py
+++ msb/zmq_base/Subscriber.py
@@ -2,20 +2,28 @@ import zmq
 import pickle
 import json
 
+from msb.zmq_base.Config import ZMQConf
 
 class Subscriber:
-    def __init__(self, topic_or_topics, config):
+    def __init__(self, topic_or_topics):
 
-        self.config = config
+        self.config = ZMQConf()
+        # after merging with configuration branch, update of
+        # configuration through configuration file should happen here
 
         self.context = zmq.Context.instance()
         self.socket = self.context.socket(zmq.SUB)
-        self.socket.connect(config.connect_to)
+        self.socket.connect(self.config.connect_to_subscriber)
         self.subscribe(topic_or_topics)
 
         # Possible to add a switch via config here
         # unpacker = pickle
-        self.unpacker = json
+        if self.config.packer == "json":
+            self.unpacker = json
+        elif self.config.packer == "pickle":
+            self.unpacker = pickle
+        else:
+            raise Exception(f'{self.config.packer} is not a valid packer/unpacker. Please proide a valid serializer')
     
     def subscribe(self, topics):
         # Acceps single topic or list of topics
@@ -25,14 +33,18 @@ class Subscriber:
             for topic in topics:
                 self.socket.setsockopt(zmq.SUBSCRIBE, topic)
 
-
     def __del__(self):
         self.socket.close()
 
-
     def receive(self):
+        """
+        reads a message from the zmq bus and returns it
+
+        returns:
+            tuple(topic, message), where topic is a byte string identifyin the 
+            source service of the message
+        """
         (topic, message) = self.socket.recv_multipart()
         message = self.unpacker.loads(message.decode())
         return (topic, message)
-
 
