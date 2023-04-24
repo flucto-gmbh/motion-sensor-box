b856f20904618254fe237f1d20f82b8c458767af
kawindme <32780200+kawindme@users.noreply.github.com>
2023-04-24 18:23:16 +0200

    add pyproject.toml and fh in test/test_config.py
    add minimal pyproject.toml for pytest to find rootdir
    use t pytest rootdi test to find confile

--- test/test_config.py
+++ test/test_config.py
@@ -1,60 +1,78 @@
-import pytest 
+import pytest
+
 
 def test_config_import():
     from msb.imu.IMUConfig import IMUConf
+
     print(IMUConf)
 
-def test_config_yaml_parse():
-    example_config_fpath = "../config/msb/imu.yaml"
+
+def test_config_yaml_parse(pytestconfig):
+    root_dir = pytestconfig.rootdir
+    example_config_fpath = root_dir + "/config/msb/imu.yaml"
     from msb.config.parse import read_yaml_config_file
+
     conf = read_yaml_config_file(example_config_fpath)
     print(conf)
 
+
 def test_imu_config_class():
     from msb.imu.IMUConfig import IMUConf
+
     imu_conf = IMUConf()
     print(imu_conf)
+
 
 def test_update_config_class():
     from msb.imu.IMUConfig import IMUConf
     from msb.config.parse import update_config
+
     updated_config = {
-        'verbose' : True,        
+        "verbose": True,
     }
     imu_conf = IMUConf()
     imu_conf.verbose = False
     update_config(imu_conf, updated_config)
-    assert imu_conf.verbose == updated_config['verbose']
+    assert imu_conf.verbose == updated_config["verbose"]
+
 
 def test_attribute_does_not_exists_warning():
     from msb.imu.IMUConfig import IMUConf
     from msb.config.parse import update_config
+
     updated_config = {
-        'does_not_exist' : 42,        
+        "does_not_exist": 42,
     }
     imu_conf = IMUConf()
     with pytest.warns(UserWarning):
         update_config(imu_conf, updated_config)
-    #assert imu_conf.verbose == updated_config['verbose']
+    # assert imu_conf.verbose == updated_config['verbose']
+
 
 def test_argparse_key_value():
     import argparse
     from msb.config.cmdline import KeyValue
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--params', nargs='*', action = KeyValue)
+    parser.add_argument("--params", nargs="*", action=KeyValue)
     args = parser.parse_args("--params key=value1".split())
-    assert args.params['key'] == "value1"
+    assert args.params["key"] == "value1"
+
 
 def test_argparse():
     from msb.config.cmdline import get_cmdline
     from msb.imu.IMUConfig import IMUConf
     from msb.config.parse import update_config
+
     imu_conf = IMUConf()
-    updated_config = { 'verbose' : True, }
-    args = get_cmdline("--print-stdout --params sample_rate_div=31".split()) # should return: {'print_stdout' : True}
+    updated_config = {
+        "verbose": True,
+    }
+    args = get_cmdline(
+        "--print-stdout --params sample_rate_div=31".split()
+    )  # should return: {'print_stdout' : True}
     update_config(imu_conf, updated_config)
     update_config(imu_conf, args)
-    assert imu_conf.verbose == updated_config['verbose']
-    assert imu_conf.print_stdout == args['print_stdout']
-    assert imu_conf.sample_rate_div == int(args['sample_rate_div'])
-
+    assert imu_conf.verbose == updated_config["verbose"]
+    assert imu_conf.print_stdout == args["print_stdout"]
+    assert imu_conf.sample_rate_div == int(args["sample_rate_div"])
