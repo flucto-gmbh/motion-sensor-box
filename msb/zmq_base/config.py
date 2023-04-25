18877e4444dc8479b0e9509ad8bce0a6a987d1c8
kawindme <32780200+kawindme@users.noreply.github.com>
2023-04-25 19:27:45 +0200

    split ZMQConf into ZMQConf and child class PublisherSubscriberConf, and rename attributes
    Strictly speaking the conf value `packer` is not an ZMQ config value,
    but only a config value for Publishers and Subscribers.
    As the broker will use the ZMQConf as well it is split into two,
    and the PublisherSubscriberConf will extend ZMQConf.
    Additionally rename producer_port->publisher_port,
    consumer_port->subscriber_port and address->interface,
    as this follows the ZMQ terminology.

---
+++ msb/zmq_base/config.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from msb.config.MSBConfig import MSBConf
+
+
+class ZMQConf(MSBConf):
+    protocol: str = "tcp"
+    interface: str = "127.0.0.1"
+    publisher_port: int = 5555
+    subscriber_port: int = 5556
+
+    @property
+    def publisher_address(self):
+        return f"{self.protocol}://{self.interface}:{self.publisher_port}"
+
+    @property
+    def subscriber_address(self):
+        return f"{self.protocol}://{self.interface}:{self.subscriber_port}"
+
+
+class PublisherSubscriberConf(ZMQConf):
+    packer: str = "json"
+
+    @property
+    def producer_connection(self): # TODO rename/use publisher_address
+        return f"{self.protocol}://{self.interface}:{self.publisher_port}"
+
+    @property
+    def consumer_connection(self): # TODO rename/use subscriber_address
+        return f"{self.protocol}://{self.interface}:{self.subscriber_port}"
