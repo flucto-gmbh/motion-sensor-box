3b2e3e7e633f0e50d9727da3651029699ec2fbc8
kawindme <32780200+kawindme@users.noreply.github.com>
2023-04-25 19:02:13 +0200

    return updated config in msb.config.update_config

--- msb/config/parse.py
+++ msb/config/parse.py
@@ -1,9 +1,13 @@
-from dataclasses import dataclass
-import json
 import os
 import sys
 import warnings
 import yaml
+
+from typing import TypeVar
+
+from msb.config import MSBConf
+
+ConfigType = TypeVar("ConfigType", bound=MSBConf)
 
 
 def get_msb_config_filepath(config_filename : str = "msb.conf") -> str:
@@ -11,26 +15,28 @@ def get_msb_config_filepath(config_filename : str = "msb.conf") -> str:
         config_filepath = os.path.join(os.environ['MSB_CONFIG'], config_filename)
     except Exception as e:
         print(f"could no get MSB_CONFIG from PATH: {e}")
-        sys.exit()
+        sys.exit() # TODO use 1 or the error str as exit value
     if not os.path.isfile(config_filepath):
         print('not a file: {config_filepath}!')
         sys.exit()
     return config_filepath
 
+
 def read_yaml_config_file(config_fpath : str) -> dict:
     with open(config_fpath, 'r') as config_filehandle:
         return yaml.safe_load(config_filehandle)
 
-def update_config(config_object, config_conffile : dict):
-    for config_key, config_value in config_conffile.items():
+
+def update_config(config: ConfigType, config_dict : dict) -> ConfigType:
+    for config_key, config_value in config_dict.items():
         # get expected type of element from config_object:
-        if not hasattr(config_object, config_key):
+        if not hasattr(config, config_key):
             warnings.warn(f"no such configuration parameter: {config_key}, skipping")
             continue
-        cast_func = type(config_object[config_key])
+        cast_func = type(config[config_key])
         try:
-            config_object[config_key] = cast_func(config_value)
+            config[config_key] = cast_func(config_value)
         except Exception as e:
-            print(f'failed to cast {config_value} to {type(config_object[config_key])}: {e}. skipping')
+            print(f'failed to cast {config_value} to {type(config[config_key])}: {e}. skipping')
             continue
-
+    return config
