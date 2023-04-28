8065e0b8848ec95dd6f540fb96712919c967351f
kawindme <32780200+kawindme@users.noreply.github.com>
2023-04-28 20:33:00 +0200

    update TimeSeriesLogger to use new config format and expect new json payloads

--- msb/fusionlog/config.py
+++ msb/fusionlog/config.py
@@ -1,12 +1,71 @@
 from msb.config import MSBConf
 
 
+_default_key_order = {
+    "att": [
+        "datetime",
+        "epoch",
+        "uptime",
+        "roll",
+        "roll_corr",
+        "pitch",
+        "pitch_corr",
+    ],
+    "imu": [
+        "datetime",
+        "epoch",
+        "uptime",
+        "acc_x",
+        "acc_y",
+        "acc_z",
+        "rot_x",
+        "rot_y",
+        "rot_z",
+        "mag_x",
+        "mag_y",
+        "mag_z",
+        "temp",
+    ],
+    "gps": [
+        "datetime",
+        "epoch",
+        "uptime",
+        "gps_mode",
+        "gps_timestamp",
+        "latitude",
+        "longitude",
+        "altitude",
+        "altitude_msl",
+        "track",
+        "mag_track",
+        "mag_var",
+        "speed",
+    ],
+    "pwr": [
+        "datetime",
+        "epoch",
+        "uptime",
+        "load-1",
+        "load-5",
+        "load-15",
+        "mem-available",
+        "swap-cached",
+    ],
+}
+
+
 class FusionlogConf(MSBConf):
     data_dir: str = "/home/msb/msb_data"
-    datetime_fmt: str = "%Y%m%dT%H%M%S%z"
-    """temporal length in seconds of each logfile: 1h (3600s)"""
+
+    filename_datetime_fmt: str = "%Y%m%dT%H%M%S%z"
+
     logfile_interval: int = 3600
-    # """for testing purposes"""
+    """temporal length in seconds of each logfile: 1h (3600s)"""
+
     # logfile_interval : 10
-    """maximum number of logfiles before oldest logfiles get overwritten"""
+    # """for testing purposes"""
+
     max_logfiles: int = 100
+    """maximum number of logfiles before oldest logfiles get overwritten"""
+
+    key_order: dict[str, list[str]] = _default_key_order
