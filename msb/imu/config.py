de469bb46e877cff1f5f444988aec02069def6de
kawindme <32780200+kawindme@users.noreply.github.com>
2023-04-28 21:07:38 +0200

    import enums for IMUConf that where moved to ICM20948_settings.py

--- msb/imu/config.py
+++ msb/imu/config.py
@@ -1,28 +1,27 @@
-import argparse
-from dataclasses import dataclass
-from enum import Enum, auto
-import json
+from msb.config.MSBConfig import MSBConf
 
-from numpy import format_float_scientific
+from msb.imu.ICM20948.ICM20948_settings import (
+    AccelerationFilter,
+    AccelerationSensitivity,
+    GyroFilter,
+    GyroSensitivity,
+)
 
-from msb.config.MSBConfig import MSBConfig, MSBConf
-from msb.imu.ICM20948.ICM20948_settings import ICM20948_SETTINGS
+
 class IMUConf(MSBConf):
     """
 
     configuration class for the inertial measurement unit (imu) service
 
     """
-    verbose : bool = False
-    print_stdout : bool = False
-    i2c_address : int = 104 
-    sample_rate_divisor : int = 30 # 1125/(1+sample_rate_divisor) Hz where sample_rate_divisor is 0, 1, 2,…4095
-    acc_filter : AccelerationFilter = AccelerationFilter.DLPF_OFF
-    gyr_filter : GyroFilter = GyroFilter.DLPF_OFF
-    acc_sensitivity : AccelerationSensitivity = AccelerationSensitivity.G_2
-    gyr_sensitivity : GyroSensitivity = GyroSensitivity.DPS_250
 
-if __name__ == "__main__":
-    config = IMUConfig()
-    if config.verbose:
-        print(json.dumps(config.__dict__, indent=4))
+    verbose: bool = False
+    print_stdout: bool = False
+    i2c_address: int = 104
+    sample_rate_divisor: int = (
+        30  # 1125/(1+sample_rate_divisor) Hz where sample_rate_divisor is 0, 1, 2,…4095
+    )
+    acc_filter: AccelerationFilter = AccelerationFilter.DLPF_OFF
+    gyr_filter: GyroFilter = GyroFilter.DLPF_OFF
+    acc_sensitivity: AccelerationSensitivity = AccelerationSensitivity.G_2
+    gyr_sensitivity: GyroSensitivity = GyroSensitivity.DPS_250
