0cc391b149ad0b3480604deca9d992ac8f64faae
felix <github@felix.weiler.cc>
2023-05-07 15:31:01 +0200

    Add imports to optr init

---
+++ msb/optr/__init__.py
@@ -0,0 +1,4 @@
+from .tracker import OpticalFlowTracker, OptrConfig
+from .video import video_source, gui_split, add_draw_func
+from .video.VideoFormatSource import video_format_source
+from .filter import filter_generator, add_filter_func
