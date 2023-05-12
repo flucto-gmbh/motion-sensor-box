34e1a72c0a2ba9d0b6b01a8ec84f235e589f2bec
kawindme <32780200+kawindme@users.noreply.github.com>
2023-05-12 21:41:33 +0200

    update config and imu.yaml and set default filter

--- msb/imu/config.py
+++ msb/imu/config.py
@@ -16,11 +16,11 @@ class IMUConf(MSBConf):
     i2c_bus_num: int = 1
     i2c_address: int = 104
     sample_rate_divisor: int = (
-        30  # 1125/(1+sample_rate_divisor) Hz where sample_rate_divisor is 0, 1, 2,…4095
+        31  # 1125/(1+sample_rate_divisor) Hz where sample_rate_divisor is 0, 1, 2,…4095
     )
-    acc_filter: AccelerometerFilter = AccelerometerFilter.DLPF_OFF
-    gyr_filter: GyroscopeFilter = GyroscopeFilter.DLPF_OFF
+    acc_filter: AccelerometerFilter = AccelerometerFilter.DLPF_473
+    gyr_filter: GyroscopeFilter = GyroscopeFilter.DLPF_361
     acc_sensitivity: AccelerometerSensitivity = AccelerometerSensitivity.G_2
     gyr_sensitivity: GyroscopeSensitivity = GyroscopeSensitivity.DPS_250
     precision: int = 4
-    polling: bool = True
+    polling: bool = False
