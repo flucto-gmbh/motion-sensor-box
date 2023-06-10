283fc6250c065d21d3fcf7a3db2e6d51948e1b63
Aljoscha Sander <aljoscha@flucto.tech>
2023-06-10 18:21:58 +0000

    pulled reimplemented picamera2 service from picamera2 branch in

--- msb/camera/msb_camera.py
+++ msb/camera/msb_camera.py
@@ -1,15 +1,13 @@
+import cv2
 import datetime
 import json
 import os
-import picamera
+from picamera2 import MappedArray, Picamera2
+from picamera2.encoders import H264Encoder
 import signal
 import sys
 import time
 
-<<<<<<< HEAD
-timeformat = "%Y%m%dT%H%M%S%z"
-split_interval = 60 # seconds
-=======
 from msb.camera.config import CameraConf
 from msb.config import load_config
 
@@ -19,36 +17,28 @@ origin = (0, 30)
 font_name = cv2.FONT_HERSHEY_SIMPLEX
 scale = 1
 thickness = 2
->>>>>>> f726958 (fixed requested changes)
 
-from msb.camera.CameraConfig import CameraConfig
 
 def signal_handler(sig, frame):
-    print("msb_piletrack.py exit")
+    print("msb_camera.py exit")
     sys.exit(0)
+
 
 def get_datetime() -> datetime.datetime:
     return datetime.datetime.fromtimestamp(time.time(), tz=datetime.timezone.utc)
 
-def get_datetime_str(timeformat : str = "%Y%m%dT%H%M%S.%f%z", timestamp=None) -> str:
-    if timestamp:
-        return timestamp.strftime(timeformat)
-    else:
-        return get_datetime().strftime(timeformat)
 
-def exit_handler(sig, frame):
-    global camera
-    camera.stop_recording()
-    print(f"test_camera.py received signal {sig}, exiting")
-    sys.exit()
+def get_datetime_str(timestamp: datetime.datetime, config: CameraConf) -> str:
+    return timestamp.strftime(config.timeformat)
 
-def get_new_fhandle(ts : datetime.datetime, folder : str = 'curdir', prefix : str = "msb-video") -> str:
-    if folder == 'curdir':
-        output_dir = os.path.abspath(os.path.curdir)
-    else:
-        output_dir = folder
-        if not os.path.isdir(output_dir):
-            raise Exception(f"no such file or directory: {folder}")
+
+def get_new_fhandle(timestamp: datetime.datetime, config: CameraConf) -> str:
+    ts_str = get_datetime_str(timestamp, config)
+    if not os.path.isdir(config.video_dir):
+        try:
+            os.makedirs(config.video_dir, exists_ok=True)
+        except Exception as e:
+            print(f"failed to create output directory: {config.video_dir}")
             sys.exit()
     return os.path.join(config.video_dir, f"{ts_str}_{config.serial_number.lower()}.h264")
 
@@ -59,9 +49,8 @@ def apply_timestamp(request):
     with MappedArray(request, "main") as m:
         cv2.putText(m.array, timestamp, origin, font_name, scale, color, thickness)
 
->>>>>>> f726958 (fixed requested changes)
 
-def setup_camera(camera_config : CameraConfig):
+def setup_camera(config: CameraConf):
     """
     setup raspberry pi camera for recording
     """
@@ -73,8 +62,23 @@ def setup_camera(camera_config : CameraConfig):
     camera.pre_callback = apply_timestamp
     return camera
 
-def msb_camera(camera_config : CameraConfig):
-    if camera_config.verbose:
+
+def setup_globals(config: CameraConf):
+    global color
+    global origin
+    global scale
+    global thickness
+    global timeformat
+
+    color = tuple(config.font["color"])
+    origin = tuple(config.font["origin"])
+    scale = config.font["scale"]
+    thickness = config.font["thickness"]
+    timeformat = config.timeformat
+
+
+def msb_camera(config: CameraConf):
+    if config.verbose:
         print("starting camera recording")
         print("msb_camera configuration:")
         print(f"{config.to_json()}")
@@ -87,14 +91,18 @@ def msb_camera(camera_config : CameraConfig):
         print(f"entering endless loop")
     while True:
         ts = get_datetime()
-        if (ts - last_ts).seconds >= split_interval:
-            print(f"{ts}: {split_interval} s of recording, switching")
-            camera.split_recording(get_new_fhandle(ts, prefix=camera_config.serialnumber))
+        if (ts - last_ts).seconds >= config.rollover_period:
+            if config.verbose:
+                print(
+                    f"{ts}: {config.rollover_period} s of recording, rolling over in new file"
+                )
+            camera.stop_recording()
+            camera.start_recording(encoder, get_new_fhandle(get_datetime(), config))
             last_ts = ts
-        camera.annotate_text = get_datetime_str(timestamp=ts)
-        camera.wait_recording(0.05)
+
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
-    camera_config = CameraConfig()
-    msb_camera(camera_config)
+    config = load_config(CameraConf(), "camera")
+    setup_globals(config)
+    msb_camera(config)
