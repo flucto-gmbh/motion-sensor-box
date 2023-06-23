91a8e188c87ca7ada00c43575fce4c10f6b280ff
felix <github@felix.weiler.cc>
2023-06-23 13:01:11 +0800

    Fix lenght of signal, add two subscribers to serial forwarder

--- msb/serial/config.py
+++ msb/serial/config.py
@@ -4,9 +4,9 @@ from msb.config.MSBConfig import MSBConf
 
 
 @dataclass
-class FugroSerialConfig:
+class FugroSerialConfig(MSBConf):
     # example output 1685854601.000000,0.010,0.020,0.100,0.010,0.004,1.200
-    format: str = "{}{}"
+    port: str = "/dev/serial0"
     baudrate: int = 9600
-    databits: int = 8
+    bytesize: int = 8
     parity: int = 1
--- msb/serial/publisher.py
+++ msb/serial/publisher.py
@@ -1,6 +1,11 @@
 import serial
 
 from msb.serial.config import FugroSerialConfig
+from msb.mqtt.subscriber import MQTT_Subscriber
+from msb.mqtt.subscriber import get_default_subscriber as get_mqtt_subscriber
+from msb.zmq_base.Subscriber import Subscriber as ZMQ_Subscriber
+from msb.zmq_base.Subscriber import get_default_subscriber as get_zmq_subscriber
+from msb.config import load_config
 
 
 def pad(n_pre, n_post, string):
@@ -15,6 +20,8 @@ def pad(n_pre, n_post, string):
 
     f = front.rjust(n_pre, "0")
     b = back.ljust(n_post, "0")
+    if len(b) > n_post:
+        b = b[:n_post]
 
     return f + "." + b
 
@@ -22,44 +29,89 @@ def pad(n_pre, n_post, string):
 # timestamp,abs_pile_velocity,rel_pile_velocity,vessel_roll,vessel_pitch,vessel_yaw,pile_distance_travelled
 # 1685854601.000000,0.010,0.020,0.100,0.010,0.004,1.200
 def serial_packer(input_dict) -> bytes:
-    ts = pad(10, 6, input_dict["timestamp"])
-    ap = pad(1, 3, input_dict["abs_pile_velocity"])
-    rp = pad(1, 3, input_dict["rel_pile_velocity"])
-    roll = pad(1, 3, input_dict["vessel_roll"])
-    pitch = pad(1, 3, input_dict["vessel_pitch"])
-    yaw = pad(1, 3, input_dict["vessel_yaw"])
-    dist = pad(1, 3, input_dict["pile_distance_travelled"])
+    ts = pad(10, 6, input_dict["epoch"])
+    ap = pad(1, 3, input_dict["velocity_x"] * 6e4)
+    rp = pad(1, 3, input_dict["distance_x"] * 6e4)
+    roll = pad(1, 3, input_dict["roll"])
+    pitch = pad(1, 3, input_dict["pitch"])
+    yaw = pad(1, 3, input_dict["yaw"])
+    dist = pad(1, 3, input_dict["sum_distance_x"])
+    # ts = pad(10, 6, input_dict["epoch"])
+    # ap = pad(1, 3, input_dict["abs_pile_velocity"])
+    # rp = pad(1, 3, input_dict["rel_pile_velocity"])
+    # roll = pad(1, 3, input_dict["vessel_roll"])
+    # pitch = pad(1, 3, input_dict["vessel_pitch"])
+    # yaw = pad(1, 3, input_dict["vessel_yaw"])
+    # dist = pad(1, 3, input_dict["pile_distance_travelled"])
 
     return f"{ts},{ap},{rp},{roll},{pitch},{yaw},{dist}"
+    # return f"{ts:10.6f},{ap:1.3f},{rp:1.3f},{roll:1.3f},{pitch:1.3f},{yaw:1.3f},{dist:1.3f}"
 
 
 class SerialPublisher:
     def __init__(self, config: FugroSerialConfig):
         self.config = config
         self.packer = serial_packer
-        self.connect()
+        # self.connect()
 
     def connect(self):
         self.serial = serial.Serial(
             port=self.config.port,
             baudrate=self.config.baudrate,
-            bitesize=self.config.bitesize,
+            bytesize=self.config.bytesize,
             parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE
+            stopbits=serial.STOPBITS_ONE,
         )
 
-    def send(self, topic, message):
+    def send(self, message):
         payload = self.packer(message)
         self.serial.write(payload)
         self.serial.flush()
 
     def __del__(self):
-        if not self.serial_is_open():
+        if not hasattr(self, "serial"):
+            return
+        if not self.serial.is_open():
             return
         self.serial.flush()
         self.serial.close()
 
 
 class SerialForwarder:
-    def __init__(self, subscriber, publisher):
-        pass
+    def __init__(
+        self, subscriber: MQTT_Subscriber | ZMQ_Subscriber, publisher: SerialPublisher
+    ):
+        self.subs = subscriber
+        self.pub = publisher
+
+    """
+    Wait for message and forward
+    """
+    def forward_message(self):
+        collected = {}
+        for sub in self.subs:
+            topic, data = sub.receive()
+            collected.update(data)
+
+        payload = self.pub.packer(collected)
+        print(payload)
+        # self.pub.send(data)
+
+    """
+    Enter loop and continuously forward messages
+    """
+    def sub_pub_loop(self):
+        while True:
+            self.forward_message()
+
+
+def main():
+    subscriber_opt = get_mqtt_subscriber("/+/opt")
+    subscriber_rpy = get_mqtt_subscriber("/+/rpy")
+    serial_config = load_config(
+        FugroSerialConfig(), "fugro-config", read_commandline=False
+    )
+    publisher = SerialPublisher(serial_config)
+
+    forwarder = SerialForwarder([subscriber_opt, subscriber_rpy], publisher)
+    forwarder.sub_pub_loop()
