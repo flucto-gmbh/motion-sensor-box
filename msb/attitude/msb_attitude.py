6f3f30ae5ac40957a938a3269fb504cefca76fdd
felix <github@felix.weiler.cc>
2023-06-26 14:11:52 +0800

    Black format msb_attitude

--- msb/attitude/msb_attitude.py
+++ msb/attitude/msb_attitude.py
@@ -1,4 +1,3 @@
-
 import signal
 import sys
 
@@ -8,22 +7,29 @@ from msb.attitude.filters import ComplementaryFilter
 from msb.zmq_base.Publisher import Publisher, get_default_publisher
 from msb.zmq_base.Subscriber import Subscriber, get_default_subscriber
 
+
 def signal_handler(sig, frame):
     print(f"received {sig} msb_attitude.py exit")
     sys.exit(0)
 
+
 class AttitudeService:
-    def __init__(self, config: AttitudeConf, subscriber: Subscriber, publisher: Publisher):
+    def __init__(
+        self, config: AttitudeConf, subscriber: Subscriber, publisher: Publisher
+    ):
         self.subscriber = subscriber
         self.publisher = publisher
         self.config = config
-        self.filter = ComplementaryFilter(gain=self.config.gain, exp_gain=self.config.exp_gain)
+        self.filter = ComplementaryFilter(
+            gain=self.config.gain, exp_gain=self.config.exp_gain
+        )
 
     def run(self):
         while True:
             _, imu_data = self.subscriber.receive()
             rpy_data = self.filter.update(imu_data)
             self.publisher.send(self.config.topic, rpy_data)
+
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
