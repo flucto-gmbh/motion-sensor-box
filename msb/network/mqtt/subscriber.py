a3b8fea1833cb6dff49e265e2a0bccec9f603476
felix <github@felix.weiler.cc>
2023-07-01 15:04:23 +0800

    Improvement: Switch from list FIFO to simpleQueue.

--- msb/network/mqtt/subscriber.py
+++ msb/network/mqtt/subscriber.py
@@ -1,37 +1,12 @@
 from __future__ import annotations
 from time import sleep
 from threading import Lock
+from queue import SimpleQueue
 
 from msb.config import load_config
 from .mqtt_base import MQTT_Base
 from .config import MQTTconf
 from .packer import unpacker_factory
-
-
-class MessageStack:
-    """
-    FIFO stack for incoming MQTT messages.
-    """
-    def __init__(self, max_size):
-        self._container = list()
-        self._max_size = max_size
-
-    def push(self, message):
-        """
-        Add new message, remove oldest message if max_size is exceeded.
-        """
-        self._container.append(message)
-        if len(self._container) > self._max_size:
-            self._container.pop(0)
-
-    def pop(self):
-        """
-        Return oldest saved item.
-        """
-        return self._container.pop(0)
-
-    def __len__(self):
-        return len(self._container)
 
 
 class MQTT_Subscriber(MQTT_Base):
@@ -44,11 +19,10 @@ class MQTT_Subscriber(MQTT_Base):
 
     def __init__(self, topics, config: MQTTconf):
         super().__init__(config)
-        self._message_stack = MessageStack(max_size=self.config.max_saved_messages)
+        self._message_queue = SimpleQueue(max_size=self.config.max_saved_messages)
         self.subscribe(topics)
         self.client.on_message = self._on_message
         self.unpacker = unpacker_factory(config.packstyle)
-        self._lock = Lock()
 
     def _subscribe_single_topic(self, topic: bytes | str):
         if isinstance(topic, bytes):
@@ -85,17 +59,9 @@ class MQTT_Subscriber(MQTT_Base):
         Returns:
             tuple(topic: bytes, message: dict): the message received
         """
-        timeout = 0
-        blocking_time = 0.01
-
-        while len(self._message_stack) == 0:
-            sleep(blocking_time)
-            timeout += blocking_time
-            if timeout > self.config.timeout_s:
-                raise TimeoutError("No message received")
-
-        with self._lock:
-            mqtt_message = self._message_stack.pop()
+        mqtt_message = self._message_queue.get(
+            block=True, timeout=self.config.timeout_s
+        )
 
         topic = mqtt_message.topic.encode("utf-8")
         message_returned = self.unpacker(mqtt_message.payload.decode())
@@ -103,8 +69,7 @@ class MQTT_Subscriber(MQTT_Base):
 
     # callback to add incoming messages onto stack
     def _on_message(self, client, userdata, message):
-        with self._lock:
-            self._message_stack.push(message)
+        self._message_queue.put(message)
 
         if self.config.verbose:
             print(f"Topic: {message.topic}")
