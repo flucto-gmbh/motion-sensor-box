bee7b3b205f68c31d88b0acd44031b7762e12a38
felix <github@felix.weiler.cc>
2023-07-11 19:29:40 +0800

    Mock tests with mqtt pub and sub

--- test/mqtt/test_mqtt.py
+++ test/mqtt/test_mqtt.py
@@ -1,97 +1,43 @@
-import pytest
-import paho.mqtt.client as mqtt_client
-import time
-import ssl
-
-from msb.network.mqtt.mqtt_base import MQTT_Base
-from msb.network.mqtt.config import MQTTconf
+from unittest.mock import MagicMock
 
 
-def packer_func(data):
-    now = int(time.time_ns())
-    msg = f"turbine turbine={data['data']} {now}"
-    return msg
+def test_mqtt_publisher(mocker):
+    from msb.network.mqtt.publisher import MQTT_Publisher
+    from msb.network.mqtt.config import MQTTConf
+    import json
+
+    # Create a mock instance of the MQTT client
+    mock_paho_mqtt = MagicMock()
+
+    # Patch the paho-mqtt client module and return the mock instance
+    mocker.patch("msb.network.mqtt.mqtt_base.mqtt_client", return_value=mock_paho_mqtt)
+
+    # Call the function that uses paho-mqtt client
+
+    conf = MQTTConf(broker="localhost", port=1)
+    pub = MQTT_Publisher(conf)
+
+    pub.send("test", {"epoch": 1})
+
+    # Assert that the connect() function was called with the expected arguments
+    mock_paho_mqtt.connect.assert_called_once_with("localhost", 1)
+    mock_paho_mqtt.publish.assert_called_once_with("test", json.dumps({"epoch": 1}), qos=0, retain=False)
 
 
-def setup_config():
-    config = MQTTconf(
-        broker="localhost", user="", password="", port=1883, qos=0, ssl=False
-    )
-    return config
+def test_mqtt_subscriber(mocker):
+    from msb.network.mqtt.subscriber import MQTT_Subscriber
+    from msb.network.mqtt.config import MQTTConf
 
+    # Create a mock instance of the MQTT client
+    mock_paho_mqtt = MagicMock()
 
-def get_client(config):
-    # MQTT callbacks
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print(f"MQTT node connected to {config.broker}:{config.port}")
-        else:
-            print("Connection failed!")
-            assert False
+    mocker.patch("msb.network.mqtt.mqtt_base.mqtt_client", return_value=mock_paho_mqtt)
 
-    client = mqtt_client.Client()
-    client.username_pw_set(config.user, config.password)
-    client.on_connect = on_connect
+    conf = MQTTConf(broker="localhost", port=1)
+    sub = MQTT_Subscriber("test", conf)
 
-    if config.ssl:
-        # By default, on Python 2.7.9+ or 3.4+,
-        # the default certification authority of the system is used.
-        client.tls_set(
-            ca_certs="/etc/ssl/certs/ca-certificates.crt", cert_reqs=ssl.CERT_REQUIRED
-        )
-        print("using ssl")
-    else:
-        print("not using ssl")
+    mock_paho_mqtt.subscribe.assert_called_once_with("test", 0)
 
-    client.connect(config.broker, config.port)
-    time.sleep(1)
+    sub.subscribe(["test1", "test2"])
+    mock_paho_mqtt.subscribe.assert_called_with([("test1", 0), ("test2", 0)])
 
-    return client
-
-
-def test_mqtt_node():
-    from msb.mqtt.msb_mqtt import MQTT_Base
-
-    mqtt = MQTT_Base(setup_config())
-
-    for x in range(10):
-        mqtt.send("mqtttest", {"data": x})
-        time.sleep(0.1)
-
-
-@pytest.mark.skip
-def test_zmq_to_mqtt_loop():
-    from msb.mqtt.publisher import MQTT_Publisher
-    from msb.zmq_base import get_default_subscriber
-
-    config = setup_config()
-    config.packstyle = "default"
-
-    zmq_sub = get_default_subscriber(b"")
-
-    pub = MQTT_Publisher(config, zmq_sub)
-
-    for x in range(10):
-        pub._zmq_to_mqtt()
-
-
-@pytest.mark.skip
-def test_mqtt_server_connection():
-    config = setup_config()
-    mqtt_node = MQTT_Base(config)
-    mqtt_node.pack = packer_func
-    # client = get_client(config)
-    print(f"connected to {config.broker}:{config.port}")
-    time.sleep(1)
-
-    for x in range(10):
-        mqtt_node.send("mqtttest", {"data": x})
-        # payload = f"turbine test/test={float(x)} {time.time_ns()}"
-        # client.publish(topic="mqtttest", payload=payload, qos=1)
-
-        # print(f"t = mqtttest m = {payload} to {config.broker}:{config.port}")
-        # time.sleep(1)
-
-
-if __name__ == "__main__":
-    test_mqtt_server_connection()
