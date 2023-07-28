f69e92166ec128868ce7761633b6220bab56a485
felix <github@felix.weiler.cc>
2023-07-29 01:38:30 +0800

    Remove parity setting from serial config.

--- msb/network/serial/config.py
+++ msb/network/serial/config.py
@@ -5,9 +5,7 @@ from msb.config.MSBConfig import MSBConf
 
 @dataclass
 class SerialConf(MSBConf):
-    # example output 1685854601.000000,0.010,0.020,0.100,0.010,0.004,1.200
     port: str = "/dev/serial0"
     baudrate: int = 9600
     bytesize: int = 8
-    parity: int = 1
     encoding: str = "ascii"
