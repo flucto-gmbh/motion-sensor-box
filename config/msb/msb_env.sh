54676c35f0ac52eedc55d8a687d6ce7305072fbd
aljoscha sander <aljoscha@flucto.tech>
2023-08-02 13:28:21 +0000

    fixed broken config path

--- config/msb/msb_env.sh
+++ config/msb/msb_env.sh
@@ -2,7 +2,7 @@
 
 export MSB="${HOME}/motion-sensor-box"
 export MSB_SRC="${MSB}/src"
-export MSB_CONFIG_DIR="${HOME}/.config/msb/conf.d/"
+export MSB_CONFIG_DIR="${HOME}/.config"
 export MSB_DATA="${HOME}/msb_data"
 export PYTHONPATH="/usr/local/lib/python3/dist-packages/:$PYTHONPATH"
 
