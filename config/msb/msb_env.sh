08505d6ef1aab1f153b6ece76a5ccd70202dd490
Aljoscha Sander <aljoscha.sander@mailbox.org>
2023-08-17 17:40:52 +0200

    changed MSB_SRC to MSB/msb

--- config/msb/msb_env.sh
+++ config/msb/msb_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export MSB="${HOME}/motion-sensor-box"
-export MSB_SRC="${MSB}/src"
+export MSB_SRC="${MSB}/msb"
 export MSB_CONFIG_DIR="${HOME}/.config"
 export MSB_DATA="${HOME}/msb_data"
 export PYTHONPATH="/usr/local/lib/python3/dist-packages/:$PYTHONPATH"
