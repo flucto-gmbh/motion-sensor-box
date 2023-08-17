1f54a5bcf99aed49060974f717028b308ef9c23c
P-Bent <prellbergb@yahoo.de>
2023-08-17 20:05:47 +0800

    changed filetype to file extension

--- scripts/calibration_image.sh
+++ scripts/calibration_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Enter filename without filetype"
+echo "Enter filename without file extension"
 read filename
 
 libcamera-still -o "/home/msb/"$filename".jpg" -t 3000 --width 1920 --height 1080 --autofocus-mode manual --lens-position 0.0
