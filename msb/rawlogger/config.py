1e57572ddfb74fc07c1334c22d217e77dbe22a26
P-Bent <bent@uni-bremen.de>
2023-08-19 05:02:35 +0200

    improved rawlogger, blacklisting instead of whitelisting

--- msb/rawlogger/config.py
+++ msb/rawlogger/config.py
@@ -1,7 +1,7 @@
 from msb.config import MSBConf
 
-
 class RawLoggerConf(MSBConf):
+
     data_dir: str = "/home/msb/msb_data/rawlog"
 
     filename_datetime_fmt: str = "%Y%m%dT%H%M%S%z"
@@ -9,5 +9,8 @@ class RawLoggerConf(MSBConf):
     rollover_period: int = 3600
     """Temporal length in seconds of each logfile: 1h (3600s)"""
 
-    topics: list[str] = ["imu", "gps"]
+    excluded_topics: list[str] = ["test"]
+    """The topics to be excluded from log"""
+
+    topics: list[str] = [""]
     """The topics to log."""
--- msb/rawlogger/msb_rawlogger.py
+++ msb/rawlogger/msb_rawlogger.py
@@ -4,7 +4,7 @@ import sys
 from msb.config import load_config
 from msb.network.zmq.config import ZMQConf
 from msb.network.zmq.subscriber import ZMQRawSubscriber
-from msb.rawlogger.config import RawLoggerConf
+from msb.rawlogger.config import RawLoggerConf 
 from msb.rawlogger.rawlogger import RawLogger
 
 
@@ -15,15 +15,16 @@ def signal_handler(sig, frame):
 
 
 class RawLoggerService:
-    def __init__(self, config: RawLoggerConf, subscriber: ZMQRawSubscriber):
+    def __init__(self, config: RawLoggerConf, subscriber: ZMQRawSubscriber, excluded_topics):
+        self.excluded_topics = excluded_topics
         self.config = config
         self.subscriber = subscriber
         self.logger = RawLogger(config)
-
+        print(self.logger,"self.logger")
         if self.config.verbose:
             print(self.config)
 
-    def get_data(self) -> tuple[bytes, bytes]:
+    def get_data(self,excluded_topics) -> tuple[bytes, bytes]:
         while True:
             try:
                 (topic, data) = self.subscriber.receive()
@@ -32,20 +33,25 @@ class RawLoggerService:
                 continue
             yield topic, data
 
-    def run(self):
-        for topic, data_raw in self.get_data():
-            if self.config.print_stdout:
-                print(f"{topic.decode('utf-8')} : {data_raw.decode('utf-8')}")
 
-            data = b'{"' + topic + b'":' + data_raw + b"}\n"
-            self.logger.write(data)
+    def run(self,excluded_topics):
+        for topic, data_raw in self.get_data(excluded_topics):
+            if topic.decode('utf-8') in excluded_topics:
+                pass
+            elif self.config.print_stdout:
+                print(f"{topic.decode('utf-8')} : {data_raw.decode('utf-8')}")
+            else:
+                data = b'{"' + topic + b'":' + data_raw + b"}\n"
+                self.logger.write(data)
 
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
     raw_logger_config = load_config(RawLoggerConf(), "rawlogger")
-    print(raw_logger_config)
     zmq_config = load_config(ZMQConf(), "zmq")
+    excluded_topics = raw_logger_config.excluded_topics
     subscriber = ZMQRawSubscriber(raw_logger_config.topics, zmq_config)
-    raw_logger_service = RawLoggerService(raw_logger_config, subscriber)
-    raw_logger_service.run()
+    raw_logger_service = RawLoggerService(raw_logger_config, subscriber,excluded_topics)
+    raw_logger_service.run(excluded_topics)
+
+
